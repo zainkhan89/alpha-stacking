8aeff9bf002f08c6ebd958677b0d5515dfa89745
Zain Ul Abideen Khan <67059106+zainkhan89@users.noreply.github.com>
2023-10-30 18:37:25 +0500

    Update RWT.sol

--- RWT.sol
+++ RWT.sol
@@ -8,11 +8,3 @@ contract RWT is ERC20, Ownable {
     constructor() ERC20("RewardToken", "RWT") {
         // _mint(msg.sender, 100000 * 10 ** decimals());
     }
-
-    function mint(address to, uint256 amount) external  {
-        _mint(to, amount);
-    }
-}
-
-
-// mint the token upon claiming rewards to the user address
